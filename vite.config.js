092648077425a4674195a22880ce4e9609ab41d1
Muhammad Usman <118838228+usmantariqdev@users.noreply.github.com>
2023-08-16 13:31:59 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/",
-    plugins: [vue()],
-    build: {
-        rollupOptions: {
-            external: [
-                '/images/illustration-features-tab-1.svg',
-            ],
-        },
-    },
+    plugins: [vue()]
 });
